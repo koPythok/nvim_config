89d91f53be624a5e6f5f3fdb52104205814db196
Vladimir Kopytok <kopytokv@gmail.com>
2023-11-16 20:12:31 +0400

    Remove Highlight on yank

--- init.lua
+++ init.lua
@@ -40,18 +40,6 @@ P.S. You can delete this when you're done too. It's your config now :)
 
 require("kopythok")
 
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
